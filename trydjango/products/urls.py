ed23e6d47fc4df0b38794fc69d184d8e20eea759
jeffsto <jeffsto@gmail.com>
2020-08-29 12:03:19 -0400

    Reverse URLs

--- trydjango/products/urls.py
+++ trydjango/products/urls.py
@@ -11,6 +11,6 @@ app_name = 'products'
 urlpatterns = [
     path('<int:my_id>/', dynamic_lookup_view, name='product'),
     path('<int:my_id>/delete/', product_delete_view, name='product-delete'),
-    path('', product_list_view, name="product-list")
+    path('', product_list_view, name="product-list"),
     path('create/', product_create_view, name='product-create'),
 ]
