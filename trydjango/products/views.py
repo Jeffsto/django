1be9ce4ee66b26c1f2348129750cb11a9dba5b1d
jeffsto <jeffsto@gmail.com>
2020-08-28 23:13:18 -0400

    Working on form validation

--- trydjango/products/views.py
+++ trydjango/products/views.py
@@ -16,7 +16,14 @@ def product_detail_view(request):
     return render(request, "products/product_detail.html", context)
 
 def product_create_view(request):
-    my_form = RawProductForm(request.POST)
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            #Data is good
+            print(my_form.cleaned_data)
+        else:
+            print(my_form.errors)
     context = {
         "form": my_form
     }
